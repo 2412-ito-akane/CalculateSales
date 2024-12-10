71fd599c73d87a8bfff1d17a71aa6b22376114f2
ito_akane <ito.akane@alhinc.jp>
2024-12-10 16:59:44 +0900

    ご確認お願いいたします。

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,6 +39,49 @@ public class CalculateSales {
 
 		// ※ここから集計処理を作成してください。(処理内容2-1、2-2)
 
+		//全ファイルをfilesに格納
+		//ディレクトリのパス→args[0](実行構成で設定済み)
+		File[] files = new File(args[0]).listFiles();
+
+		//ArrayListにrcdファイルを格納するための変数
+		ArrayList<File> rcdFiles = new ArrayList<>();
+
+		//全てのファイル名(拡張子含む)を取得する
+		for(int i=0;i < files.length;i++) {
+
+			//ファイル名の判定をしたい
+			if((files[i].getName()).matches("^[0-9]{8}.rcd$")) {
+
+				//trueの時、Listに追加
+				rcdFiles.add(files[i]);
+			}
+		}
+
+		//rcdFilesから1個1個のファイルの情報を取得したい
+		readFile()
+
+		for (int i=0; i < rcdFiles.size();i++) {
+
+
+				BufferedReader br = null;
+				File rcdFile = new File(rcdFiles[i]);
+				FileReader fr = new FileReader(rcdFile);
+				br = new BufferedReader(fr);
+
+				String line;
+
+				while((line = br.readLine())!=null) {
+					//改行ごとに配列に格納
+					List<>
+
+					System.out.println(br);
+				}
+
+
+		}
+
+
+
 
 
 		// 支店別集計ファイル書き込み処理
@@ -45,6 +89,7 @@ public class CalculateSales {
 			return;
 		}
 
+	}
 	}
 
 	/**
@@ -66,34 +111,34 @@ public class CalculateSales {
 
 			String line;
 			// 一行ずつ読み込む
+			//lineには1行分の情報が入る
 			while((line = br.readLine()) != null) {
 				// ※ここの読み込み処理を変更してください。(処理内容1-2)
 
-//				カンマで区切って配列に格納
+				//カンマで区切って配列に格納
 				String[] items = line.split(",");
 
+				//配列に格納されているか確認
+				for (int i =0;i<items.length; i++) {
+					System.out.println(i+":"+items[i]);
+				}
+
+				//支店が増えても自動で対応できるようにする
+				//配列に格納されている[0]を、putメソッドの引数として使いたい
+				int i = 0;
+				while  (i<items.length) {
+					//格納した要素の[0]と[1]を支店コードと支店名を保持するマップへ
+					branchNames.put(items[0], items[1]);
+
+					//格納した要素の[0]を支店コードと売上額を保持するマップへ
+					branchSales.put(items[0], 0L);
+					i++;
+				}
 
 
-//				2つのMapに追加する情報をputの引数として指定
-				branchNames.put("001", "札幌支店");
-				branchSales.put("001", 0L);
 
-				branchNames.put("002", "仙台支店");
-				branchSales.put("002", 0L);
-
-				branchNames.put("003", "東京支店");
-				branchSales.put("003", 0L);
-
-				branchNames.put("004", "名古屋支店");
-				branchSales.put("004", 0L);
-
-				branchNames.put("005", "大阪支店");
-				branchSales.put("005", 0L);
-
-
-
-				System.out.println(branchNames);
-				System.out.println(line);
+//				System.out.println(branchNames);
+//				System.out.println(line);
 			}
 
 		} catch(IOException e) {
